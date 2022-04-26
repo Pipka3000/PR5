c04ca6e499cc724ac55bf8d1e817d2c300595cfc
Гура Никита Станиславович гр. ИС(ПРО)-31 <is12756@stud.KHPK.RU>
2022-04-26 11:10:03 +0700

    first commit

---
+++ PR5/AddEditPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PR5
+{
+
+    //Код для кнопки сохранить
+    
+    public partial class AddEditPage : Window
+    {
+        private @class _currentClass = new @class();
+        public AddEditPage()
+        {
+            InitializeComponent();
+            DataContext = _currentClass;
+
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e) //Метод для сохранения данных
+        {
+            StringBuilder errors = new StringBuilder(); //StringBuilder выделяет какую-то часть памяти
+            if (string.IsNullOrWhiteSpace(_currentClass.name_class.ToString())) // Метод IsNullOrWhiteSpace проверяет текст на null или текст состоит только из пробелов.
+                errors.AppendLine("Нужно указать ID"); // Добавляет знак завершения строки по умолчанию в конец текущего объекта StringBuilder
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            {
+                Gura_PR5Entities.Get_context().@class.Add(_currentClass);
+
+
+                try
+                {
+                    Gura_PR5.Get_context().SaveChanges(); //Используется для сохранения изменений даннных в таблице
+                    MessageBox.Show("Информация сохранена"); //Если условие не выполняется, то выходит окно с этим сообщением
+                    MainWindow mainWindow = new MainWindow(); //Объявление метода "Show"
+                    mainWindow.Show();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка" + ex.Message.ToString()); //Выполняется в том случае, если не выполнился thy
+                }
+            }
+        }
+
+    }
+}
---
+++ PR5/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace PR5
+{
+
+    //Код для кнопки добавить
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DGridClass.ItemsSource = Gura_PR5Entities.Get_context().@class.ToList();
+        }
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e) //Что будет происходить при нажатии на эту кнопку
+        {
+            AddEditPage addEditPage = new AddEditPage();
+            addEditPage.Show();
+            this.Close(); 
+        }
+
+        private void Window_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(Visibility == Visibility.Visible)
+            {                                                                                              // Reload повторно загружает сущность из базы данных, перезаписывая все значения свойств значениями из базы данных.
+                Gura_PR5Entities.Get_context().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());  //ToList принудительно выполняет вычисление запроса и возвращает List<T> значение, содержащее результаты запроса. 
+                DGridClass.ItemsSource = Gura_PR5Entities.Get_context().@class.ToList(); //ItemsSource указывает на коллекцию, содержащую все объекты, которые будут показаны в списке
+            }
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
